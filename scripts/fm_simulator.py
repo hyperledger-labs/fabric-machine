5cd55290941526e3500f6421a310153bd9a10eb4
Haris Javaid <harisj@xilinx.com>
2021-11-03 00:22:45 +0800

    [Feature] Adds support for input/output in Fabric Machine simulator.
    Signed-off-by: Haris Javaid <harisj@xilinx.com>

--- scripts/fm_simulator.py
+++ scripts/fm_simulator.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+#
+# Copyright Xilinx Inc. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
 # This program implements an adhoc simulator for Blockchain/Fabric Machine.
 #
 # Run this simulator as:
@@ -7,11 +11,27 @@
 # To see all the configurable options, use -h option.
 
 import argparse
+from datetime import datetime
 from collections import deque
 from concurrent import futures
 import copy
 import random
+import re
 import time
+
+# Regular expressions to extract various types of data.
+block_info_re = re.compile(
+    '(.*) UTC(.*)Committed block \[(\d+)\] with (\d+) transaction\(s\) in (\d+)us ' +
+    '\(state_validation=(\d+)us block_and_pvtdata_commit=(\d+)us state_commit=(\d+)us\)')
+# block_info_re = re.compile(
+#     '(.*) UTC(.*)Committed block \[(\d+)\] with (\d+) transaction\(s\) in (\d+)ms ' +
+#     '\(state_validation=(\d+)ms block_and_pvtdata_commit=(\d+)ms state_commit=(\d+)ms\)')
+
+
+def extract_block_info(line):
+    match = block_info_re.match(line)
+    if match:
+        return {'block': match.group(3), 'txs': match.group(4)}
 
 
 def print_config(config):
@@ -134,19 +154,6 @@ def finish_tx(config, txs, tx_num):
             print('finished tx{0}'.format(tx_num))
 
 
-# def tx_vscc_v1(config, threads, tx_fifo, block_num, num_txs):
-#     start_time = time.perf_counter()
-
-#     fs = []
-#     for i in range(num_txs):
-#         tx_ends = tx_fifo.popleft()
-#         fs.append(threads.submit(verify_ends, config, i, tx_ends))
-#     futures.wait(fs)
-
-#     end_time = time.perf_counter()
-#     print('block{0} {1:0.4f}ms'.format(block_num, (end_time - start_time) * 1000))
-
-
 # Implements the tx_vscc stage with v1 architecture.
 #
 # Arguments:
@@ -209,6 +216,11 @@ def tx_vscc_v1(config, threads, tx_fifo, block_num, num_txs):
 # Returns:
 #   Time spent in msecs
 def run_simulation(config, block_fifo, tx_fifo):
+    peer_log, pl = config['peer_log'], None
+    if peer_log:
+        pl = open(peer_log, 'w')
+        pl.write('Fabric Machine Simulator v1\n\n')
+
     with futures.ThreadPoolExecutor(max_workers=config['vscc_threads']) as vscc_threads:
         config['version'] = 1
         print_config(config)
@@ -232,10 +244,48 @@ def run_simulation(config, block_fifo, tx_fifo):
             block_latency = 2*ecdsa_engine_latency + vscc_latency + mvcc_commit_latency
             total_latency += block_latency
 
+            write_block_info(pl, {'block': b, 'txs': block_txs, 'latency': int(round(block_latency))})
             print('block{0} {1:0.1f} + {2:0.1f} + {3:0.1f} + {4:0.1f} = {5:0.1f}ms'.format(
                 b, ecdsa_engine_latency, ecdsa_engine_latency, vscc_latency, mvcc_commit_latency, block_latency))
-        print('average block latency {0:0.1f}ms'.format(total_latency/blocks))
-        print('average throughput {0:0.3f}tps'.format(total_txs/(total_latency/1000)))
+
+        print('average block latency: {0:0.1f}ms'.format(total_latency/blocks))
+        print('average throughput: {0:0.3f}tps'.format(total_txs/(total_latency/1000)))
+
+    if pl:
+        pl.close()
+
+
+# Writes block information in peer's log format.
+#
+# Arguments:
+#   peer_log (File): file object to write to.
+#   block_stats (dict): block stats to write.
+#
+# Returns:
+#   None
+def write_block_info(peer_log, block_stats):
+    if not peer_log:
+        return
+
+    block_num = block_stats['block']
+    block_txs = block_stats['txs']
+
+    peer_log.write('.....{0} ... START Block Validation for block [{1}]\n'.format(
+        datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'), block_num))
+    peer_log.write('.....{0} ... Validated block [{1}] in 0us\n'.format(
+        datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'), block_num))
+    peer_log.write('.....{0} ... Block [{1}] transaction validation flags: '.format(
+        datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'), block_num))
+
+    words = block_txs // 32
+    first_word = block_txs - (32 * words)
+    peer_log.write('0X{:08X} '.format(2**first_word - 1))  # All transactions marked as valid.
+    for i in range(words):
+        peer_log.write('0XFFFFFFFF ')
+
+    peer_log.write(('\n.....{0} ... Committed block [{1}] with {2} transaction(s) in {3}us '
+        '(state_validation=0us block_and_pvtdata_commit=0us state_commit=0us)\n\n').format(
+        datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'), block_num, block_stats['txs'], block_stats['latency']))
 
 
 # Generates input for simulation based on the provided parameters.
@@ -248,14 +298,41 @@ def run_simulation(config, block_fifo, tx_fifo):
 # Returns:
 #   None
 def generate_input(config, block_fifo, tx_fifo):
-    for b in range(config['blocks']):
-        bs = config['block_size']
-        block_fifo.append(bs)
+    blocks_file = config['blocks_file']
+    if blocks_file:
+        with open(blocks_file, 'r') as bf:
+            blocks = 0
+            blocks_size = 0
 
-        # For each transaction of a block, the number of endorsements are picked based on the
-        # provided probability distribution for endorsements.
-        for t in random.choices(config['ends_pop'], weights=config['ends_prob'], k=bs):
-            tx_fifo.append(t)
+            for line in bf:
+                line = line.strip()
+
+                block_info = extract_block_info(line)
+                if block_info:
+                    bs = int(block_info['txs'])
+                    blocks += 1
+                    blocks_size += bs
+                    block_fifo.append(bs)
+
+                    # For each transaction of a block, the number of endorsements are picked based on the
+                    # provided probability distribution for endorsements.
+                    # TODO(harisj): use endorsements from the input file when present.
+                    for t in random.choices(config['ends_pop'], weights=config['ends_prob'], k=bs):
+                        tx_fifo.append(t)
+
+            # Overwrite config data.
+            config['blocks'] = blocks
+            config['block_size'] = blocks_size / blocks
+
+    else:
+        for b in range(config['blocks']):
+            bs = config['block_size']
+            block_fifo.append(bs)
+
+            # For each transaction of a block, the number of endorsements are picked based on the
+            # provided probability distribution for endorsements.
+            for t in random.choices(config['ends_pop'], weights=config['ends_prob'], k=bs):
+                tx_fifo.append(t)
 
     if config['verbose']:
         print("\n=== Number of endorsements of all transactions ===")
@@ -279,15 +356,17 @@ def check_ends_params(ends_pop, ends_prob):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Adhoc simulator to estimate block validation latency in block_processor module (of Blockchain Machine).')
+    parser = argparse.ArgumentParser(description='Adhoc simulator to estimate block validation latency in Blockchain/Fabric Machine.')
     parser.add_argument('-v', dest='verbose', action='store_true', help='Enable verbose output.')
     parser.add_argument('--ecdsa-engine-latency', type=int, default=360, dest='ecdsa_engine_latency', action='store', help='Latency of an ecdsa engine in msecs (default: 360 ms).')
     parser.add_argument('--tx-vscc-ecdsa-engines', type=int, default=2, dest='tx_vscc_ecdsa_engines', action='store', help='Number of ecdsa engines per transaction for vscc operation. Used with --vscc-threads.')
     parser.add_argument('--vscc-threads', type=int, default=8, dest='vscc_threads', action='store', help='Number of transactions to process in parallel during vscc operation.')
+    parser.add_argument('--blocks-file', type=str, default=None, dest='blocks_file', action='store', help='File containing blocks data in peer\'s log format for simulation. Overrides --blocks and --blocks-size.')
     parser.add_argument('--blocks', type=int, default=5, dest='blocks', action='store', help='Number of blocks used in simulation.')
     parser.add_argument('--block-size', type=int, default=100, dest='block_size', action='store', help='Number of transactions per block.')
     parser.add_argument('--ends', type=str, default='2:1.0', dest='ends', action='store', help='Number of endorsements per transaction. Format is "e1,e2,e3:p1,p2,p3" where e1 endorsements are generated with p1 probability in a block. When --ends-params is specified, then the format is "e" which is internally converted to endorsements [e//2, e, e+1, (e+1)*2].')
     parser.add_argument('--ends-params', type=str, default='', dest='ends_params', action='store', help='Parameters for generating endorsements per transaction. Format is "alpha,beta" which is internally converted to endorsement probabilities [beta/2, alpha, (1-beta-alpha), beta/2]. Used with --ends.')
+    parser.add_argument('--peer-log', type=str, default=None, dest='peer_log', action='store', help='File for writing simulation output in peer\'s log format.')
     args = parser.parse_args()
 
     if args.ends_params:
@@ -311,10 +390,12 @@ if __name__ == '__main__':
         'mvcc_commit_latency': 10,  # msecs.
         'tx_vscc_ecdsa_engines': args.tx_vscc_ecdsa_engines,
         'vscc_threads': args.vscc_threads,
+        'blocks_file': args.blocks_file,
         'blocks': args.blocks,
         'block_size': args.block_size,
         'ends_pop': ends_pop,
-        'ends_prob': ends_prob
+        'ends_prob': ends_prob,
+        'peer_log': args.peer_log
     }
     block_fifo = deque()
     tx_fifo = deque()
